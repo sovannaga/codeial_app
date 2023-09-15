b229a7b591ab2910c891ba076a75101c080be927
sovannaga <sovan2003naga@gmail.com>
2023-09-15 20:47:16 +0530

    Display Post and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,34 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
-}
+const Post = require('../models/post');
+
+module.exports.home = async function (req, res) {
+    // try {
+    //     // console.log(req.cookies);
+    //     // res.cookie('user_id', 25);
+    //     const posts = await Post.find({});
+    //     console.log(posts);
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // } catch (err) {
+    //     console.log('Error in fetching posts', err);
+    //     return res.status(500).send('Internal Server Error');
+    // }
+
+    //populate the user of each post
+    try {
+        const posts = await Post.find({}).populate('user').exec();
+        res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    } catch (err) {
+        // Handle any errors here
+        console.error(err);
+        // Send an error response
+        res.status(500).send('Internal Server Error');
+    }
+    
+};
+
+//module.exports.actionName = async function(req, res){}
