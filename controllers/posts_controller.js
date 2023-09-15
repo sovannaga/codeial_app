71fafbbcb7231f79c757ac5ba493d54485debb26
sovannaga <sovan2003naga@gmail.com>
2023-09-15 20:56:16 +0530

    saving post to the database

---
+++ controllers/posts_controller.js
@@ -0,0 +1,16 @@
+const Post = require('../models/post');
+
+module.exports.create = async function (req, res) {
+    try {
+        console.log(req.user._id);
+        const post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+
+        return res.redirect('back');
+    } catch (err) {
+        console.log('error in creating a post', err);
+        return res.status(500).send('Internal Server Error');
+    }
+};
