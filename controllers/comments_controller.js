b70673d51c29f61fb56601624acdb2c7c283294c
sovannaga <sovan2003naga@gmail.com>
2023-09-16 11:21:44 +0530

    add controller and router for creating the comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,19 +2,24 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function( err, post){
-        if(post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            }, function(err, comment){
-                post.comments.push(comment);
-                post.save();
-
-                res.redirect('/');
-            });
+module.exports.create = async function(req, res) {
+    try {
+      const post = await Post.findById(req.body.post);
+      
+        if (post) {
+          const comment = await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+          });
+    
+          post.comments.push(comment);
+          await post.save();
+    
+          res.redirect('/');
         }
-    });
-}
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  }
